a02ed04b3346bad859cba656651903192925027b
MDeFoor <36637425+MDeFoor@users.noreply.github.com>
2022-01-27 10:44:30 -0600

    initial files for jack

---
+++ Models/CredentialModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nVestia.Models
+{
+    public class CredentialModel
+    {
+        public string API_KEY { get; set; }
+        public string API_SECRET { get; set; }
+        public string API_URL { get; set; }
+    }
+}
